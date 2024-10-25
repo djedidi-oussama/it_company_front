ef071c2cbae9fdb096d3fbd68e3ea457413d4457
ubuntu <djdidoussama@gmail.com>
2024-10-25 09:20:43 +0100

    fix design

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-6xl mx-auto`}
       >
         {/* Wrap the entire application with ReduxProvider */}
         <ReduxProvider>
