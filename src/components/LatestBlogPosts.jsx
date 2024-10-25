15883e0a4d159d62f4d2e39b1ef7eff37b58a52a
ubuntu <djdidoussama@gmail.com>
2024-10-25 10:56:21 +0100

    fix error

--- src/components/LatestBlogPosts.jsx
+++ src/components/LatestBlogPosts.jsx
@@ -8,7 +8,7 @@ const LatestBlogPosts = ({blogs}) => {
                 <h2 className='text-3xl md:text-5xl font-bold text-center mb-10' style={{ color: 'var(--text-dark)' }}>
                     Latest Blog Posts
                 </h2>
-                <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+                <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
                     {blogs.map(post => (
                         <div 
                             key={post._id} 
